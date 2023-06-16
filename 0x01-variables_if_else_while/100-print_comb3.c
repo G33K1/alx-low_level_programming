8729d4effa8d549fe996175bf9dcc3aa233995ac
G33K1 <johnjomo30@gmail.com>
2023-06-16 15:15:29 -0700

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 	int m;
 
-	for (n = 48; n < 57; n++)
+	for (n = 48; n < 56; n++)
 	{
 		for (m = 49; m < 57; m++)
 		{
@@ -19,7 +19,7 @@ int main(void)
 			{
 				putchar(n);
 				putchar(m);
-				if (n == 56 || m == 57)
+				if (n != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
