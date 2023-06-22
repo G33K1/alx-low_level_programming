ae747d9f82c21f2bdefcbb8d119faa239f58d55f
G33K1 <johnjomo30@gmail.com>
2023-06-22 12:13:37 -0700

    supper, digit & mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of ints
+ * @a: integer one
+ * @b: integer two
+ *
+ * Return: always 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
